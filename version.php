93ca3d13e97da7f27e5980f243bf27ac3f321301
manuelBojacaJ <manuel@buendata.com>
2025-10-03 15:21:51 -0500

    feat: final mvp ui

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_socialcert';
-$plugin->release = '0.1.0';
+$plugin->release = '1.0.0';
 $plugin->version = 2025100204;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
